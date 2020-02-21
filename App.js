c869c42de96fdc314fd3935c685180ecc4393f70
I Gusti Made Erwin Ardiantha <igmerwina@gmail.com>
2020-02-21 15:37:45 +0700

    Perbaikan App.js. Hapus file-file yang tidak diperlukan

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import Drawer from './src/navigators/drawer-navigator';
-import LoginScreen from './src/screens/LoginScreen';
 import { createAppContainer } from 'react-navigation';
-import StackNavigators from './src/navigators/StackNavigator'
-
-const AppContainer = createAppContainer(StackNavigators);
 
 export default class App extends React.Component {
 
   render() {
     return (
       <Drawer/>
-      // <LoginScreen />
-      // <AppContainer />
     );
   }
 }
