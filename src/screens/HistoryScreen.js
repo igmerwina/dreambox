c53e0f6645909ae1ba7cc0e4e117bd846f778ecb
I Gusti Made Erwin Ardiantha <igmerwina@gmail.com>
2020-03-08 16:34:33 +0700

    add history transaksi dengan Spinner dan sudah ambil dari API

--- src/screens/HistoryScreen.js
+++ src/screens/HistoryScreen.js
@@ -2,13 +2,17 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 import React, { Component } from "react";
 import { MenuButton, Logo } from "../components/header/header";
 import axios from "axios";
-import { 
-  Card, 
-  CardItem, 
-  Container, 
-  Content, 
-  List, 
-  ListItem
+import {
+  Card,
+  CardItem,
+  Container,
+  Content,
+  List,
+  ListItem,
+  Spinner,
+  H2,
+  Left,
+  Right
 } from "native-base";
 
 export default class HistoryScreen extends React.Component {
@@ -24,20 +28,22 @@ export default class HistoryScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      transactionHistory: []
+      transactionHistory: [],
+      loading: false
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true })
     fetch("http://mydreambox.herokuapp.com/dreambox/listtransaksi/74")
       .then(response => response.json())
       .then((responseJson) => {
-        console.log(responseJson)
         this.setState({
+          loading: false,
           transactionHistory: responseJson.data
         })
       })
-      .catch(error => console.log(error)) //to catch the errors if any
+      .catch(error => console.log(error))
   }
 
 
@@ -46,16 +52,21 @@ export default class HistoryScreen extends React.Component {
     return (
       <Container>
         <Content style={{ padding: 16 }}>
+          <H2 style={styles.title}>Daftar Transaksi</H2>
           <Card>
             <CardItem>
-              <FlatList
-                padding={10}
-                data={this.state.transactionHistory}
-                renderItem={({ item }) =>
-                  <View style={{ height: 50 }}>
-                    <Text style={{ height: 50 }}>{item.target_tanggal}</Text>
-                    <View style={{ height: 1, backgroundColor: 'gray' }}></View>
-                  </View>
+              {this.state.loading && (<Spinner style={styles.spinner} color="green" />)}
+              <List
+                dataArray={this.state.transactionHistory}
+                renderRow={(item, key) =>
+                  <ListItem>
+                    <Left>
+                      <Text>{item.tanggal_debit}</Text>
+                    </Left>
+                    <Right>
+                      <Text style={{ fontSize: 10 }}>+ {item.jumlah_gram_bulanan} gram</Text>
+                    </Right>
+                  </ListItem>
                 }
               />
             </CardItem>
@@ -71,5 +82,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
+  },
+  title: {
+    margin: 5,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#65A898'
+  },
+  spinner: {
+    marginLeft: "50%"
   }
 });
